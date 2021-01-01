44d1a0716ee006efcc8896be7c53522db6fad625
Jakub <jakub.syk@gmail.com>
2021-01-01 18:01:06 -0500

    Chapter 3 - added a reservationService class that gets the reservation for a specific date and returns it

--- src/main/java/com/jakub/lil/learningspring/data/repository/ReservationRepository.java
+++ src/main/java/com/jakub/lil/learningspring/data/repository/ReservationRepository.java
@@ -3,5 +3,8 @@ package com.jakub.lil.learningspring.data.repository;
 import com.jakub.lil.learningspring.data.entity.Reservation;
 import org.springframework.data.repository.CrudRepository;
 
+import java.sql.Date;
+
 public interface ReservationRepository extends CrudRepository<Reservation,Long> {
+    Iterable<Reservation> findReservationByReservationDate(Date date);
 }
